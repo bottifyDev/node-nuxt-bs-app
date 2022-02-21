0c57e688aaae31262ae768ab6fe40005d12492dd
bottifyDev <bottify@yandex.ru>
2022-02-21 13:02:27 +1000

    prepare nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <li v-for="user in users" :key=user.id> {{user.name}} </li>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data: () => ({
+    users: [{'id': 1, 'name': 'ben'}, {'id': 2, 'name': 'bot'}]
+  }),
+  methods: {
+    goTo(user) {
+      this.$router.push('/users' + user)
+    }
+  }
+
+}
+</script>
